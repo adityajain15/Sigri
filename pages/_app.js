7d45ee3be3440a44da8da5695213ac2e1cf7e07e
Aditya J <ajain15@uic.edu>
2020-11-06 16:22:31 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import React from "react";
+import "../styles/globals.css";
+import BaseLayout from "../layouts/BaseLayout";
+import "tachyons";
+import "reset-css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <BaseLayout>
+      <Component {...pageProps} />
+    </BaseLayout>
+  );
 }
 
-export default MyApp
+export default MyApp;
